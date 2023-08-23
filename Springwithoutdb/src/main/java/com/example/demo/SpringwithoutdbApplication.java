824ace257386f693a28e0d609d656da2c542e041
Aruljothi S <132689076+Jothika05@users.noreply.github.com>
2023-08-23 14:21:45 +0530

    Add files via upload

---
+++ Springwithoutdb/src/main/java/com/example/demo/SpringwithoutdbApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwithoutdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringwithoutdbApplication.class, args);
+	}
+
+}
